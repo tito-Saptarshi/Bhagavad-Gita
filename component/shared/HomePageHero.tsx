f636e8b7e497b99b461b0438894f2807d5f2d2b1
tito-Saptarshi <saptarshi.tito.20@gmail.com>
2024-03-04 21:09:51 +0530

    buttons removed from hero

--- component/shared/HomePageHero.tsx
+++ component/shared/HomePageHero.tsx
@@ -17,14 +17,7 @@ const HomePageHero = () => {
             <p className="mb-8 leading-relaxed">
             Welcome to a sacred digital haven where the profound teachings of the Bhagavad Gita come alive. Immerse yourself in the timeless wisdom of this ancient scripture, as we present the complete Bhagavad Gita for you to explore and contemplate. Our mission is to make this spiritual treasure accessible to seekers around the world, fostering a journey of self-discovery, inner peace, and spiritual enlightenment. Dive into the verses that encapsulate the essence of life, duty, and the path to self-realization, and let the Bhagavad Gita be your guide on the journey to a more profound understanding of existence. Join us on this enlightening voyage through the words that have inspired generations and continue to illuminate the path to a harmonious and meaningful life.
             </p>
-            <div className="flex justify-center">
-              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                Button
-              </button>
-              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-                Button
-              </button>
-            </div>
+            
           </div>
         </div>
       </section>
